c4d24c5a93b349ce81f96ed53d3ced996cd0d877
LinQin <woshilinqin163@163.com>
2019-07-30 21:09:20 +0800

    修改导航栏

--- config/navConf.js
+++ config/navConf.js
@@ -2,30 +2,46 @@ module.exports = [{
 	text: 'Home',
 	link: '/'
 }, {
-	text: 'Java学习',
-	link: '/Java学习/'
-}, {
-	text: 'Linux',
-	link: '/Linux/'
-}, {
-	text: 'Markdown入门到放弃',
-	link: '/Markdown入门到放弃/'
-}, {
-	text: 'mysql',
-	link: '/mysql/'
-}, {
-	text: 'Spring',
-	link: '/Spring/'
-}, {
-	text: 'Spring-boot',
-	link: '/Spring-boot/'
-}, {
-	text: '复习题',
-	link: '/复习题/'
+	text: 'Category',
+	items: [{
+			text: 'Spring技术',
+			items: [{
+				text: 'Spring',
+				link: '/Spring/'
+			}, {
+				text: 'Spring-boot',
+				link: '/Spring-boot/'
+			}]
+		}, {
+			text: '数据库',
+			items: [{
+				text: 'mysql',
+				link: '/mysql/'
+			}]
+		}, {
+			text: 'Linux',
+			items: [{
+				text: 'Linux',
+				link: '/Linux/'
+			}]
+		}, {
+			text: 'Other',
+			items: [{
+					text: 'Java学习',
+					link: '/Java学习/'
+				}, {
+					text: 'Markdown入门到放弃',
+					link: '/Markdown入门到放弃/'
+				}, {
+					text: '复习题',
+					link: '/复习题/'
+				}
+
+			]
+		}
+
+	]
 }, {
 	text: '设计模式',
 	link: '/设计模式/'
-}, {
-	text: 'External',
-	link: 'https://google.com'
 }]
