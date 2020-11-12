f5e36f5a9ac3db009c05e9617386af87226785ac
LinQin <woshilinqin163@163.com>
2020-11-12 16:42:49 +0800

    feat: 取消sitemap插件

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -8,22 +8,22 @@ module.exports = {
             buttonText: "朕知道了"
         }
     },
-    'seo': {
-        siteTitle: (_, $site) => $site.title,
-        title: $page => $page.title,
-        description: $page => $page.frontmatter.description,
-        author: (_, $site) => $site.themeConfig.author,
-        tags: $page => $page.frontmatter.tags,
-        twitterCard: _ => 'summary_large_image',
-        type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
-        url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
-        image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
-        publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
-        modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
-    },
-    'sitemap': {
-        hostname: 'https://www.linqin.site'
-    },
+    // 'seo': {
+    //     siteTitle: (_, $site) => $site.title,
+    //     title: $page => $page.title,
+    //     description: $page => $page.frontmatter.description,
+    //     author: (_, $site) => $site.themeConfig.author,
+    //     tags: $page => $page.frontmatter.tags,
+    //     twitterCard: _ => 'summary_large_image',
+    //     type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
+    //     url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
+    //     image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
+    //     publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
+    //     modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
+    // },
+    // 'sitemap': {
+    //     hostname: 'https://www.linqin.site'
+    // },
     '@vssue/vuepress-plugin-vssue': {
         // set `platform` rather than `api`
         platform: 'github',
